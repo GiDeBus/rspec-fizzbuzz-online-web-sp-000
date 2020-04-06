89a6e003781e9540ca7e829d731a92682d237b87
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-06 23:10:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(x)
-  if x
+def fizzbuzz(number)
+  if number % 3 == 0 
+    #if the variable number is divisible by 3
+    return "Fizz"
+  elseif  number % 5 == 0
+    #if the variable number is divisible by 5
+    return "Buzz"
+  elseif number 3 % == 0 and number 5 % == 0
+    #if the variable number is divisible by both 3 and 5
+    return "FizzBuzz"
+  else   
 end
