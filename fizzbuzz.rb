fb8b78c204c4c98652ba54cd42767b94d5b74982
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-06 23:19:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5
+    "FizzBuzz"int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 5
     "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5
-    "FizzBuzz"
+  elsif 
   end
 end
